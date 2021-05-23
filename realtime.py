ec494ddd2848ad98c75f9b31a033a4a2c359883a
Upamanyu Das <upamanyu.das.work@gmail.com>
2021-05-23 21:17:59 +0530

    turn model into object detector

--- realtime.py
+++ realtime.py
@@ -26,7 +26,10 @@ while True:
     # drawing bounding box around face
     for (x, y, w, h) in faces:
         img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)  # displaying image with bounding box
-    cv2.imshow("face_detect", img)  # loop will be broken when 'q' is pressed on the keyboard
-    if cv2.waitKey(10) & 0xFF == ord("q"):
-        breakcap.release()
+        cv2.imshow("face_detect", img)  # loop will be broken when 'q' is pressed on the keyboard
+        if cv2.waitKey(10) & 0xFF == ord("q"):
+            break
+imcap.release()
 cv2.destroyWindow("face_detect")
+
+# %%
